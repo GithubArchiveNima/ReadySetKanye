e14ccf1f3e697a69d284483879479fa4a9270303
NimaBoscarino <nima.boscarino@gmail.com>
2019-01-07 16:35:52 -0800

    kanye clicker, instead of timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
 import React, {Component} from 'react';
 
+const Kanye = (props) => {
+  const clicks = props.clicks
+  let src = ""
+
+  if (clicks <= 0) {
+    src = "build/kanyes/100.png"
+  } else if (clicks <= 5) {
+    src = "build/kanyes/50.png"
+  } else if (clicks <= 10) {
+    src = "build/kanyes/25.png"
+  } else {
+    src = "build/kanyes/0.png"
+  }
+
+  return (
+    <img className="kanye" src={src}/>
+  )
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      clicks: 0
+    }
+
+    this.increaseKanye = this.increaseKanye.bind(this)
+  }
+
+
+  increaseKanye() {
+    console.log('INCREASE')
+    this.setState({
+      clicks: this.state.clicks + 1
+    })
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <h1>Increase Kanye!</h1>
+        <h2>{this.state.clicks} clicks</h2>
+        <button onClick={this.increaseKanye}>
+          Increase
+        </button>
+        <Kanye clicks={this.state.clicks}/>
+      </div>
     );
   }
 }
